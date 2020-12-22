6090a4d459160fff9531634fbeb4e3433053e787
Leonardo Gardini <leogardini10.1@gmail.com>
2020-12-22 12:27:56 -0300

    Incluído o carregamento das informações de acesso pelo .env

---
+++ ftp_refresher/config.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+from os import environ, path
+
+from dotenv import load_dotenv
+
+# Carrega as informações do .env
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+# Informações de acesso
+HOST = environ.get('HOST')
+USER = environ.get('USER')
+SSH_KEY = environ.get('SSH_KEY')
+REMOTE_PATH = environ.get('REMOTE_PATH')
